96cd83833c87671243fdf5186181ae719d259742
etibarliparvin <73303441+etibarliparvin@users.noreply.github.com>
2020-10-23 15:15:05 +0400

    Add files via upload

---
+++ lesson3.java
@@ -0,0 +1,10 @@
+package Lesson3;
+
+public class lesson3 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+        double c = (double) a / b; // casting
+        System.out.println(c);
+    }
+}
