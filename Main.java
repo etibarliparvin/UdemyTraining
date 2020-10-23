748c5ead9a530faca41ae7274543f918b05eaaa3
etibarliparvin <73303441+etibarliparvin@users.noreply.github.com>
2020-10-23 14:35:35 +0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package Lesson1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("My name is parvin");
+        /*
+        main - function
+        println - function
+        ("My name is parvin") - parametr
+         */
+    }
+}
