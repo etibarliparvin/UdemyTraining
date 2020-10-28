db9e6a11eb32bd9399a5d531ddc4d84caded2fa3
etibarliparvin <73303441+etibarliparvin@users.noreply.github.com>
2020-10-28 17:53:23 +0400

    Add files via upload
    Metodlari nece istifade etmek

---
+++ Ders9.java
@@ -0,0 +1,22 @@
+package Dersler;
+
+public class Ders9 {
+    public static void main(String[] args) {
+        for(int i = 0; i < 5; i++){
+            birinci.salamlama();
+        }
+        for (int i = 0; i < 6; i++){
+            ikinci.sagollawma();
+        }
+    }
+}
+class birinci{
+    public static void salamlama(){
+        System.out.println("Salamlar!!!");
+    }
+}
+class ikinci{
+    public static void sagollawma(){
+        System.out.println("Sagol!!!");
+    }
+}
