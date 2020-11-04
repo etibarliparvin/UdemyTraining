0c3087d0eebeb5d3e805d8cdc21b7fcdd4e80076
etibarliparvin <73303441+etibarliparvin@users.noreply.github.com>
2020-11-04 15:04:30 +0400

    Add files via upload
    Understanding Classes and Objects

--- Main.java
+++ Main.java
@@ -1,24 +1,22 @@
-package ClassAndObjects;
+package CalssesAndObjects;
+
+import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-        Parvin.Narmin();
-	    User.umumi = "Salam";
-	    User.foo();
-        System.out.println(User.umumi);
-        User.age = 18;
-        System.out.println(User.age);
-        User u = new User();
-        User u2 = new User();
-        u.foo2();
-        u.name = "Parvin";
-        System.out.println(u.name);
-        u2.name = "Nermin";
-        System.out.println(u2.name);
-        u2.foo2();
-        Test.change();
-        Parvin.Narmin();
-        System.out.println(User.umumi);
+        Scanner input = new Scanner(System.in);
+        int aa = input.nextInt();
+        Show kk = new Show();
+        kk.sh(aa);
+    }
+}
+class Enter{
+    public static int integ(int n){
+        return n;
+    }
+}
+class Show{
+    public void sh(int n){
+        System.out.println(Enter.integ(n));
     }
 }
