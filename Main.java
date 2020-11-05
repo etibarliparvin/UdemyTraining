ec160dc3ec877ed7bbbd7c2ab1735cc44a9d618f
etibarliparvin <73303441+etibarliparvin@users.noreply.github.com>
2020-11-05 19:21:15 +0400

    Add files via upload
    encapsulation

--- Main.java
+++ Main.java
@@ -1,21 +1,31 @@
-package Main;
+package ClassAndObjects;
+
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        int alma = 0;
-        System.out.println(alma);
-        topla(alma);
-        System.out.println(topla(alma));
+        Scanner input = new Scanner(System.in);
+        String nn = input.nextLine();
+        User u = new User();
+        u.a(nn);
+        System.out.println(u.b());
+    }
+}
+class User{
+    private String name;
+    public String surname;
+    public  int age;
+    public static String companyName;
 
+    public void a(String name){
+        this.name = name;
     }
 
-    public void foo(){
-
-    }
-
-    public static int topla(int a){
-        a += 5;
-        return a;
+    public String b(){
+        if(name.isEmpty()){
+            System.out.println("duz teyin edin!");
+        }
+        return this.name;
     }
 }
